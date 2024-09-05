5f51d99bae1df18b4b87e883d081b7d98b793c6f
Mahdi Moghassemi <mahdimoghassemi@gmail.com>
2024-09-05 12:07:55 +0330

    fixing mobile view

--- src/sections/skills/components/skill-container.tsx
+++ src/sections/skills/components/skill-container.tsx
@@ -1,5 +1,4 @@
 // MUI
-import { SxProps, Theme } from '@mui/system';
 import { Box, Typography } from '@mui/material';
 
 // ----------------------------------------------------------------------
@@ -66,38 +65,6 @@ const sections = [
 	},
 ];
 
-const styles: Record<string, SxProps<Theme>> = {
-	container: {
-		display: 'flex',
-		flexDirection: 'column',
-		alignItems: 'flex-start',
-		justifyContent: 'center',
-		height: '100vh',
-		color: '#fff',
-	},
-	content: {
-		textAlign: 'start',
-		maxWidth: '800px',
-		margin: '1rem 0 2rem 0',
-	},
-	lineContainer: {
-		maxWidth: '800px',
-		display: 'flex',
-		flexDirection: 'row',
-		gap: '2rem',
-		marginBottom: '1rem',
-	},
-	dot: {
-		width: 10,
-		height: 10,
-		borderRadius: '50%',
-		backgroundColor: 'rgba(0, 0, 0, 0.3)',
-		position: 'absolute',
-		right: '-10px',
-		marginLeft: '2px',
-	},
-};
-
 const gradients: string[] = [
 	'linear-gradient(to right, #f44336, #ff5722)',
 	'linear-gradient(to right, #9c27b0, #673ab7)',
@@ -109,7 +76,7 @@ const gradients: string[] = [
 	'linear-gradient(to right, #ff9800, #ff5722)',
 ];
 
-const lineSegment = (width: string, gradient: string): SxProps<Theme> => ({
+const lineSegment = (width: string, gradient: string) => ({
 	width: width,
 	height: 10,
 	background: gradient,
@@ -132,34 +99,73 @@ const SkillContainer = () => {
 	const bottom_rows = Array.from({ length: 3 }, () => getRandomLineCount());
 
 	return (
-		<Box sx={styles.container}>
+		<Box
+			sx={{
+				display: 'flex',
+				flexDirection: 'column',
+				alignItems: 'flex-start',
+				justifyContent: 'center',
+				flexWrap: 'wrap',
+				color: '#fff',
+				padding: { xs: 2, sm: 3, md: 4 },
+			}}>
 			{top_rows.map((lineCount, rowIndex) => (
 				<Box
 					key={rowIndex}
-					sx={styles.lineContainer}>
+					sx={{
+						flexWrap: 'wrap',
+						maxWidth: '800px',
+						display: 'flex',
+						flexDirection: 'row',
+						gap: '1rem',
+						marginBottom: '1rem',
+					}}>
 					{Array.from({ length: lineCount }).map((_, index) => (
 						<Box
 							key={index}
 							sx={lineSegment(getRandomWidth(), getRandomGradient())}>
-							{index % 2 === 0 && <Box sx={styles.dot} />}
+							{index % 2 === 0 && (
+								<Box
+									sx={{
+										width: 10,
+										height: 10,
+										borderRadius: '50%',
+										backgroundColor: 'rgba(0, 0, 0, 0.3)',
+										position: 'absolute',
+										right: '-10px',
+									}}
+								/>
+							)}
 						</Box>
 					))}
 				</Box>
 			))}
 
-			<Box sx={styles.content}>
+			<Box
+				sx={{
+					textAlign: 'start',
+					maxWidth: '800px',
+					margin: '1rem 0 2rem 0',
+				}}>
 				<Typography
 					variant='h4'
-					sx={{ color: 'rgba(0, 0, 0, 0.7)', fontWeight: 600, mb: 2 }}>
+					sx={{
+						color: 'rgba(0, 0, 0, 0.7)',
+						fontWeight: 600,
+						mb: { md: 2, sm: 1, xs: 0.5 },
+						fontSize: { xs: '1.5rem', sm: '2rem', md: '2.5rem' },
+					}}>
 					SKILLS 🛠️
 				</Typography>
 				{sections.map((section, index) => (
 					<Typography
+						key={index}
 						variant='body1'
 						sx={{
 							color: 'rgba(0, 0, 0, 0.7)',
 							display: 'flex',
 							flexWrap: 'wrap',
+							fontSize: { xs: '0.8rem', sm: '1rem', md: '1.2rem' },
 						}}>
 						⭕ {section.items.join(', ')}
 					</Typography>
@@ -169,12 +175,31 @@ const SkillContainer = () => {
 			{bottom_rows.map((lineCount, rowIndex) => (
 				<Box
 					key={rowIndex}
-					sx={styles.lineContainer}>
+					sx={{
+						maxWidth: '800px',
+						display: 'flex',
+						flexWrap: 'wrap',
+						flexDirection: 'row',
+						gap: '2rem',
+						marginBottom: '1rem',
+					}}>
 					{Array.from({ length: lineCount }).map((_, index) => (
 						<Box
 							key={index}
 							sx={lineSegment(getRandomWidth(), getRandomGradient())}>
-							{index % 2 !== 0 && <Box sx={styles.dot} />}
+							{index % 2 !== 0 && (
+								<Box
+									sx={{
+										width: 10,
+										height: 10,
+										borderRadius: '50%',
+										backgroundColor: 'rgba(0, 0, 0, 0.3)',
+										position: 'absolute',
+										right: '-10px',
+										marginLeft: '2px',
+									}}
+								/>
+							)}
 						</Box>
 					))}
 				</Box>
